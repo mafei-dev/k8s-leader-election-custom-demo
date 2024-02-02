ba5ae344713a225d2fbde5969bbf3ebd0e2c6d14
mafei <mafei.dev@gmail.com>
2024-02-02 17:11:38 +0530

    init3

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+./mvnw clean install
 docker build -t mafeidev/k8s-leader-election-custom-demo:1.0.3 .
 docker push mafeidev/k8s-leader-election-custom-demo:1.0.3
--- src/main/java/com/example/k8sleaderelectioncustomdemo/K8sLeaderElectionCustomDemoApplication.java
+++ src/main/java/com/example/k8sleaderelectioncustomdemo/K8sLeaderElectionCustomDemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.k8sleaderelectioncustomdemo;
 
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @SpringBootApplication()
 @Import(K8sConfig.class)
+@RestController
 public class K8sLeaderElectionCustomDemoApplication {
 
 
@@ -16,5 +24,28 @@ public class K8sLeaderElectionCustomDemoApplication {
                 K8sLeaderElectionCustomDemoApplication.class,
                 args);
 
+    }
+
+    @GetMapping
+    public void test() {
+        try {
+            // Load the Kubernetes config from default location or from KUBECONFIG environment variable
+
+            // Create an instance of the CoreV1Api
+            CoreV1Api api = new CoreV1Api();
+
+            // List all pods in the default namespace
+            V1PodList podList = api.listNamespacedPod("default", null, null, null, null, null, null, null, null, null, null);
+
+            // Iterate through the list of pods
+            for (V1Pod pod : podList.getItems()) {
+                System.out.println("Pod Name: " + pod.getMetadata().getName());
+                System.out.println("Namespace: " + pod.getMetadata().getNamespace());
+                System.out.println("Status: " + pod.getStatus().getPhase());
+                System.out.println("--------------");
+            }
+        } catch (ApiException e) {
+            System.err.println("Error interacting with Kubernetes API: " + e.getResponseBody());
+        }
     }
 }
