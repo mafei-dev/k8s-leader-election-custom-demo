c7a3787dd90676b7ac62fcbf991f747cc24a18c3
mafei <mafei.dev@gmail.com>
2024-02-03 17:33:44 +0530

    init16

--- src/main/java/com/example/k8sleaderelectioncustomdemo/Start.java
+++ src/main/java/com/example/k8sleaderelectioncustomdemo/Start.java
@@ -2,20 +2,29 @@ package com.example.k8sleaderelectioncustomdemo;
 
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.integration.leader.event.OnGrantedEvent;
+import org.springframework.integration.leader.event.OnRevokedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Start implements ApplicationListener<ApplicationStartedEvent> {
 
-    private final StackSagaK8sLeaderElectionConfig config;
 
-    public Start(StackSagaK8sLeaderElectionConfig config) {
-        this.config = config;
-    }
 
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
 
+    }
+
+    @EventListener
+    public void handleEvent(OnRevokedEvent event) {
+        System.out.printf("'%s' leadership revoked%n", event.getRole());
+    }
+
+    @EventListener
+    public void handleEvent(OnGrantedEvent event) {
+        System.out.printf("'%s' leadership granted%n", event.getRole());
     }
 }
